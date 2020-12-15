29760793f560879144cbf97ed4408dfafc33e33c
preethi900 <preethi900@gmail.com>
2020-12-15 08:03:36 -0800

    Adding weather Project-API Module

---
+++ Weatherproject/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res){
+  const query = req.body.cityname;
+  const apiKey = "978d9a4349d3b07c4242051a638e1ee6";
+  const unit = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey;
+  https.get(url,function(response){
+    console.log(response.statusCode);
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+      res.write("<p> The weather is currently " + description + "<p>");
+      res.write("<h1>The temperature in " +query+ " is " + temp + " degrees fahrenheit.</h1>");
+      res.write("<img src=" + iconURL + ">");
+      res.send();
+    });
+  });
+});
+
+
+app.listen(3000,function(){
+  console.log("Server started on port 3000");
+});
